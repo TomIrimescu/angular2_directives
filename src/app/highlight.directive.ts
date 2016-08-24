f1d8f80386700052b187e6ecea3489d8190bfdb6
TomIrimescu <tom@irimescu.com>
2016-08-24 11:10:46 -0400

    Added custom Attribute Directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,39 @@
 import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@angular/core';
 
-
 @Directive({
   selector: '[highlight]'
 })
 export class HighlightDirective {
+	// you could declare the property and constructor like the following:
+/*	private elementRef: ElementRef;
+	constructor(elementRef: ElementRef){
+		this.elementRef = elementRef;
+        //this approach does not work across all devices well
+		this.elementRef.nativeElement.style.backgroundColor = 'red';
+	}*/
+
+	// OR the following constructor uses dependency injection option 1
+/*	constructor(private elementRef: ElementRef) {
+		//this approach does not work across all devices well
+		this.elementRef.nativeElement.style.backgroundColor = 'red';
+	}*/
+
+	// OR the following constructor uses multiple dependency injections option 2
+/*	constructor(private elementRef: ElementRef, private renderer: Renderer) {
+		//this approach is more reliable across all devices
+		this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'red');
+	}*/
+
+	// Introduce a hover state with renderer injection
+/*	@HostListener('mouseenter') mouseover(){
+		this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'pink');
+	};
+	@HostListener('mouseleave') mouseleave(){
+		this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'white');
+	};
+	constructor(private elementRef: ElementRef, private renderer: Renderer) {}*/
+
+	// Introduce a hover state with binding
 	@HostListener('mouseenter') mouseover(){
 		this.backgroundColor = 'pink';
 	};
@@ -15,18 +44,12 @@ export class HighlightDirective {
 		return this.backgroundColor;
 	};
 	private  backgroundColor = 'white';
-	constructor() {
-	// constructor(private elementRef: ElementRef, private renderer: Renderer) {
-		// this approach does not work across all devices well
-		// this.elementRef.nativeElement.style.backgroundColor = 'red';
-		// this approach is more reliable across all devices
-		// this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'red');
-  }
+	constructor() {}
 
+	// Introduce a click event with listener passing data
 	@HostListener('click', ['$event'])
 	onClick(highlight) {
 		console.log("Event Target" + highlight.target);
 	}
-
 }
 
